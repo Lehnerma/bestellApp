c8572c56f5e6178b14b14eaddde8faec429dc74b
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2026-01-29 20:28:20 +0100

    feat - implementing the first functions to the script data adding a url element to the dishes obj. try diffrent ways to geht the right output for the dishes'

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
 function init(){
     renderDishes();
-    test();
 }
-function test(){
-    
-}
-
-
-
 
 //render dishes
-function renderDishes(){
+// function renderDishes(){
+//     const categorys = getCategory(ALL_DISHES)
+//     for (let dishIndex = 0; dishIndex < categorys.length; dishIndex++) {
+//         console.log(categorys[dishIndex]);
+//         if (categorys[dishIndex] == "starters"){
+            
+//         }
+//         if (categorys[dishIndex] == "pasta"){
 
+//         }
+//         if (categorys[dishIndex] == "pizzas") {
+
+//         }
+//         if (categorys[dishIndex] == "desserts"){
+
+//         }
+//         if (categorys[dishIndex] == "drinks"){
+
+//         }
+//     }
+// }
+function renderDishes(){
+    renderStarter()
 }
-    //split into categorys - arrays?
-function getCategory(){
-    const categorys = Object.keys(ALL_DISHES);
-    for (let catIndex = 0; catIndex < categorys.length; catIndex++) {
-      console.log(categorys[catIndex]);
-      
+function renderStarter(){
+    let starters = ALL_DISHES.starters;
+    let containerRef = document.getElementById("starters_container");
+    containerRef = "";
+    for (let dishIndex = 0; dishIndex < starters.length; dishIndex++) {
+        containerRef = getDishTemplet()
         
     }
 }
+    //split into categorys - arrays?
+function getCategory(object){
+return Object.keys(object);
+}
     //add data index
-
 
 // array basket?
 //render basket
